506993eaae8b735697c9efd08b3fa255c0dc0e29
veda <vedabezaleel@gmail.com>
2024-06-14 04:22:50 +0700

    feat: new service section

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 import JobshareIcon from "/public/assets/icon/job-share.svg";
 import AiIcon from "/public/assets/icon/ai.svg";
 import SkillIcon from "/public/assets/icon/skill.svg";
+import CvIcon from "/public/assets/icon/cv.svg";
+import ChatIcon from "/public/assets/icon/chatbot.svg";
 import { ComponentType, FunctionComponent } from "react";
 
 export default function Home() {
@@ -13,47 +15,23 @@ export default function Home() {
 
   function Masthead() {
     return (
-      <div className="w-full h-screen flex text-white relative">
+      <div className="w-full h-[70%] md:h-screen flex text-white relative">
         <Image
           src={"/assets/img/masthead.png"}
           fill
           alt="masthead"
           className="w-full h-auto absolute -z-10"
         />
-        <div className=" flex w-full h-fit flex-col self-center px-[10%] pt-16">
-          <span className="text-8xl font-bold">Quire</span>
-          <span className="my-5 max-w-[40%] font-light text-lg">
+        <div className=" flex w-full h-fit flex-col self-center px-[5%] pt-16">
+          <span className="text-6xl md:text-8xl font-bold">Quire</span>
+          <span className="my-5 max-w-[40%] font-light text-sm md:text-lg">
             Menjembatani dirimu dengan berbagai platform penyedia kerja serta
             mengenal lebih jauh preferensi dan pekerjaan yang relevan dengan
             dirimu
           </span>
-          <button className="bg-primary w-fit px-6 py-4 rounded-md font-medium text-xl">
+          <button className="bg-primary w-fit px-3 md:px-6 py-1 md:py-4 rounded-md font-medium text-sm md:text-xl">
             Cari Pekerjaan Incaranmu
           </button>
-        </div>
-      </div>
-    );
-  }
-
-  function AboutSection() {
-    return (
-      <div className="flex flex-col items-center py-20 h-[600px]">
-        <span className="font-bold text-4xl text-primary mb-12">Tentang</span>
-        <div className="flex w-fit h-fit rounded-md outline outline-2 outline-primary mx-[10%] p-[2%] justify-around">
-          <div className="flex relative w-[20%] h-auto">
-            <Image src={"/assets/icon/logo.svg"} alt="logo" fill />
-          </div>
-          <div className="flex flex-col gap-2 w-[80%] md:text-2xl text-base">
-            <span className="font-bold ">Apa itu Quire ?</span>
-            <span>
-              Quire adalah aplikasi penghubung, yang akan menjembatani job
-              seeker atau pencari kerja dengan berbagai platform dan situs yang
-              menawarkan lowongan pekerjaan. Selain itu, Quire juga membantu
-              para pencari kerja untuk mampu memahami tren dan requirement
-              pekerjaan. Serta memberikan solusi untuk memenuhi kebutuhan
-              tersebut.
-            </span>
-          </div>
         </div>
       </div>
     );
@@ -64,41 +42,52 @@ export default function Home() {
       {
         icon: JobshareIcon,
         title: "Koleksi Loker",
-        description: "description",
+        description: "Kami mengumpulkan loker dari berbagai sumber dan menyediakannya dalam satu halaman untuk anda.",
       },
       {
         icon: AiIcon,
-        title: "Dukungan Ai",
-        description: "description",
+        title: "Integrasi Ai",
+        description: "Manfaatkan AI kami untuk pencocokan kerja yang canggih dan mendapatkan tips dan trik karir yang berarti untuk Anda.",
       },
       {
         icon: SkillIcon,
-        title: "Pemberdayaan SDM",
-        description: "description",
+        title: "Skill Up",
+        description: "Dengan konten e-course kami, Anda dapat belajar dan meningkatkan keterampilan Anda, membuka lebih banyak peluang di dunia kerja",
       },
+      {
+        icon: CvIcon,
+        title: "CV",
+        description: "Optimalkan cv anda untuk setiap lowongan pekerjaan yang akan anda daftar, meningkatkan peluang lolos lebih tinggi.",
+      },
+      {
+        icon: ChatIcon,
+        title: "Chatbot",
+        description: "Personalisasi pengalaman anda dalam mencari lowongan pekerjaan dan dapatkan tips dan trik untuk meraih karir impian.",
+      }
     ];
 
     function ServiceCard({ Icon, title, description }: serviceProps) {
       return (
-        <div className="flex flex-col justify-around bg-white px-4 py-6 items-center w-80 h-96 rounded-md outline outline-4 outline-primary">
-          <div className=" w-32 h-32 text-primary">
+        <div className="flex flex-col bg-white px-4 py-6 items-start w-[30%] md:w-[24%] rounded-md hover:shadow-2xl">
+          <div className="flex w-14 p-2 text-primary bg-altBg rounded-full">
             <Icon />
           </div>
-          <div className="flex flex-col gap-4 text-center mx-auto w-[72%]">
+          <div className="flex flex-col mt-4 mb-2 w-full">
             <span className="font-semibold text-2xl">{title}</span>
-            <span>{description}</span>
+            <span className="text-sm">{description}</span>
           </div>
-        </div>
+          <span className="text-sm mt-auto hover:text-white hover:bg-primary hover:px-2 py-1 rounded-full">Baca</span>
+          </div>
       );
     }
 
     return (
-      <div className="bg-[#323233] h-[720px] flex flex-col justify-around py-16 outline outline-2 px-[10%]">
-        <div className="text-white flex flex-col gap-y-4">
-          <span className="font-semibold text-4xl">Layanan</span>
-          <span>Deskripsi</span>
+      <div className="bg-white h-[720px] flex flex-col justify-around py-16 outline outline-2 px-[5%]">
+        <div className="flex flex-col gap-y-4 items-center text-center text-primary">
+          <span className="font-semibold text-3xl md:text-6xl">Inovasi yang kami tawarkan</span>
+          <span className="text-xs md:text-base w-[60%]">Menjembatani dirimu dengan berbagai platform penyedia kerja serta mengenal lebih jauh preferensi dan pekerjaan yang relevan dengan dirimu</span>
         </div>
-        <div className="flex w-full justify-evenly text-primary mt-8">
+        <div className="flex flex-wrap w-full gap-y-12 md:gap-y-8 justify-evenly md:gap-[6%] md:justify-center text-primary mt-8">
           {serviceList.map((item) => (
             <ServiceCard
               key={item.title}
@@ -113,10 +102,9 @@ export default function Home() {
   }
 
   return (
-    <main className="w-full h-screen mb-9">
+    <div className="w-full h-screen mb-9">
       <Masthead />
-      <AboutSection />
-      <ServiceSection />\{" "}
-    </main>
+      <ServiceSection />
+    </div>
   );
 }
