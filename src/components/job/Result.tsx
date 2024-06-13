89b230084bb1ff287a1d2439aa60098afe79ef20
dylansanjaya <dylanhikmasanjaya@gmail.com>
2024-06-13 08:53:02 +0700

    Update Component Result

--- src/components/job/Result.tsx
+++ src/components/job/Result.tsx
@@ -1,43 +1,6 @@
 import Link from "next/link";
 import { Button } from "../ui/button";
 
-function JobCard({
-  image_url,
-  title,
-  poster,
-  posttime,
-  source,
-  isLiked,
-  isSaved,
-}: any) {
-  return (
-    <div className="bg-background shadow-xl rounded-xl w-96 my-4">
-      <div className="flex p-4 justify-between">
-        <div className="Content space-y-2">
-          <div className="flex space-x-2">
-            <div>
-              <div className="bg-background shadow-xl rounded-xl w-20 aspect-square">
-                {image_url}
-              </div>
-            </div>
-            <div>
-              <div>{title}</div>
-              <div>{poster}</div>
-            </div>
-          </div>
-          <div>
-            {source} - {posttime}
-          </div>
-        </div>
-        {/* <div className="flex space-x-2">
-          <div>{isLiked ? "Liked" : "notLiked"}</div>
-          <div>{isSaved ? "Saved" : "notSaved"}</div>
-        </div> */}
-      </div>
-    </div>
-  );
-}
-
 const jobs = [
   {
     id: 1,
@@ -91,9 +54,126 @@ const jobs = [
   },
 ];
 
-const totalJobs = jobs.length;
+const jobDetails = [
+  {
+    id: 1,
+    image_url: "image1.jpg",
+    title: "Software Engineer",
+    poster: "Google LLC",
+    location: "Mountain View, CA",
+    short_detail: "Develop scalable web applications.",
+    type: "Full-time",
+    posttime: "3 Hours ago",
+    link: "https://google.com/careers",
+    description:
+      "As a Software Engineer at Google, you will be working on cutting-edge technologies and developing scalable web applications to enhance user experience.",
+  },
+  {
+    id: 2,
+    image_url: "image2.jpg",
+    title: "Graphic Designer",
+    poster: "Freelance",
+    location: "Remote",
+    short_detail: "Create stunning graphics for various clients.",
+    type: "Contract",
+    posttime: "4 Hours ago",
+    link: "https://dribbble.com/freelance-jobs",
+    description:
+      "We are looking for a creative Graphic Designer to work on multiple projects for our clients. You should have a strong portfolio and experience in various design tools.",
+  },
+  {
+    id: 3,
+    image_url: "image3.jpg",
+    title: "Data Scientist",
+    poster: "Facebook Inc.",
+    location: "Menlo Park, CA",
+    short_detail: "Analyze large datasets to derive insights.",
+    type: "Full-time",
+    posttime: "2 Days ago",
+    link: "https://facebook.com/careers",
+    description:
+      "Join our Data Science team to analyze and interpret complex data sets, derive actionable insights, and contribute to our data-driven culture.",
+  },
+  {
+    id: 4,
+    image_url: "image4.jpg",
+    title: "Product Manager",
+    poster: "Amazon",
+    location: "Seattle, WA",
+    short_detail: "Lead the development of new products.",
+    type: "Full-time",
+    posttime: "1 Week ago",
+    link: "https://amazon.jobs",
+    description:
+      "As a Product Manager at Amazon, you will be responsible for guiding the development of new products from concept to launch. You will work with cross-functional teams to ensure success.",
+  },
+  {
+    id: 5,
+    image_url: "image5.jpg",
+    title: "UI/UX Designer",
+    poster: "Microsoft",
+    location: "Redmond, WA",
+    short_detail: "Design intuitive user interfaces.",
+    type: "Full-time",
+    posttime: "5 Days ago",
+    link: "https://careers.microsoft.com",
+    description:
+      "We are seeking a talented UI/UX Designer to create user-friendly interfaces for our software products. You will collaborate with developers and product managers to bring designs to life.",
+  },
+];
 
-function JobCards() {
+function JobCard({
+  jobID,
+  image_url,
+  title,
+  poster,
+  posttime,
+  source,
+  isLiked,
+  isSaved,
+  jobClickedID,
+}: any) {
+
+  let isClicked = false;
+  if (jobID == jobClickedID) {
+    isClicked = true;
+  } else {
+    isClicked = false;
+  }
+  return (
+    <div
+      className={`bg-background shadow-xl rounded-xl w-96 my-4 hover:bg-gray-200 ${
+        isClicked ? "bg-gray-200" : "bg-background"
+      }`}
+    >
+      <div className="flex p-4 justify-between">
+        <div className="Content space-y-2">
+          <div className="flex space-x-2">
+            <div>
+              <div className="bg-background shadow-xl rounded-xl w-20 aspect-square">
+                {image_url}
+              </div>
+            </div>
+            <div>
+              <div>{title}</div>
+              <div>{poster}</div>
+            </div>
+          </div>
+          <div>
+            {source} - {posttime}
+          </div>
+        </div>
+        {/* <div className="flex space-x-2">
+          <div>{isLiked ? "Liked" : "notLiked"}</div>
+          <div>{isSaved ? "Saved" : "notSaved"}</div>
+        </div> */}
+      </div>
+    </div>
+  );
+}
+
+function JobCards({ jobClickedID }: any) {
+  const totalJobs = jobs.length;
   return (
     <div className="space-y-4">
       <div>{totalJobs} Lowongan</div>
@@ -108,6 +188,8 @@ function JobCards() {
               posttime={job.posttime}
               isLiked={job.isLiked}
               isSaved={job.isSaved}
+              jobID={job.id}
+              jobClickedID={jobClickedID}
             />
           </Link>
         ))}
@@ -124,74 +206,6 @@ function JobCards() {
 }
 
 function JobDetail({ id }: any) {
-  const jobDetails = [
-    {
-      id: 1,
-      image_url: "image1.jpg",
-      title: "Software Engineer",
-      poster: "Google LLC",
-      location: "Mountain View, CA",
-      short_detail: "Develop scalable web applications.",
-      type: "Full-time",
-      posttime: "3 Hours ago",
-      link: "https://google.com/careers",
-      description:
-        "As a Software Engineer at Google, you will be working on cutting-edge technologies and developing scalable web applications to enhance user experience.",
-    },
-    {
-      id: 2,
-      image_url: "image2.jpg",
-      title: "Graphic Designer",
-      poster: "Freelance",
-      location: "Remote",
-      short_detail: "Create stunning graphics for various clients.",
-      type: "Contract",
-      posttime: "4 Hours ago",
-      link: "https://dribbble.com/freelance-jobs",
-      description:
-        "We are looking for a creative Graphic Designer to work on multiple projects for our clients. You should have a strong portfolio and experience in various design tools.",
-    },
-    {
-      id: 3,
-      image_url: "image3.jpg",
-      title: "Data Scientist",
-      poster: "Facebook Inc.",
-      location: "Menlo Park, CA",
-      short_detail: "Analyze large datasets to derive insights.",
-      type: "Full-time",
-      posttime: "2 Days ago",
-      link: "https://facebook.com/careers",
-      description:
-        "Join our Data Science team to analyze and interpret complex data sets, derive actionable insights, and contribute to our data-driven culture.",
-    },
-    {
-      id: 4,
-      image_url: "image4.jpg",
-      title: "Product Manager",
-      poster: "Amazon",
-      location: "Seattle, WA",
-      short_detail: "Lead the development of new products.",
-      type: "Full-time",
-      posttime: "1 Week ago",
-      link: "https://amazon.jobs",
-      description:
-        "As a Product Manager at Amazon, you will be responsible for guiding the development of new products from concept to launch. You will work with cross-functional teams to ensure success.",
-    },
-    {
-      id: 5,
-      image_url: "image5.jpg",
-      title: "UI/UX Designer",
-      poster: "Microsoft",
-      location: "Redmond, WA",
-      short_detail: "Design intuitive user interfaces.",
-      type: "Full-time",
-      posttime: "5 Days ago",
-      link: "https://careers.microsoft.com",
-      description:
-        "We are seeking a talented UI/UX Designer to create user-friendly interfaces for our software products. You will collaborate with developers and product managers to bring designs to life.",
-    },
-  ];
-
   const job = jobDetails.find((job) => job.id === Number(id));
 
   if (!job) {
@@ -241,7 +255,7 @@ export default function Result({
 }) {
   return (
     <div className="flex space-x-4">
-      <JobCards />
+      <JobCards jobClickedID={searchParams} />
       <JobDetail id={searchParams} />
     </div>
   );
