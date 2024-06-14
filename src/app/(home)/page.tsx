5ec5caea160f7b5f6b3dd7194fbd0f6134f193c4
veda <vedabezaleel@gmail.com>
2024-06-14 18:07:56 +0700

    feat: add about section

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -4,6 +4,7 @@ import AiIcon from "/public/assets/icon/ai.svg";
 import SkillIcon from "/public/assets/icon/skill.svg";
 import CvIcon from "/public/assets/icon/cv.svg";
 import ChatIcon from "/public/assets/icon/chatbot.svg";
+import PlussIcon from "/public/assets/icon/pluss.svg";
 import { ComponentType, FunctionComponent } from "react";
 
 export default function Home() {
@@ -74,7 +75,7 @@ export default function Home() {
           </div>
           <div className="flex flex-col mt-4 mb-2 w-full">
             <span className="font-semibold text-2xl">{title}</span>
-            <span className="text-sm">{description}</span>
+            <span className="text-sm hidden sm:block">{description}</span>
           </div>
           <span className="text-sm mt-auto hover:text-white hover:bg-primary hover:px-2 py-1 rounded-full">Baca</span>
           </div>
@@ -82,7 +83,7 @@ export default function Home() {
     }
 
     return (
-      <div className="bg-white h-[720px] flex flex-col justify-around py-16 outline outline-2 px-[5%]">
+      <div className="bg-white h-auto flex flex-col justify-around py-16 px-[5%]">
         <div className="flex flex-col gap-y-4 items-center text-center text-primary">
           <span className="font-semibold text-3xl md:text-6xl">Inovasi yang kami tawarkan</span>
           <span className="text-xs md:text-base w-[60%]">Menjembatani dirimu dengan berbagai platform penyedia kerja serta mengenal lebih jauh preferensi dan pekerjaan yang relevan dengan dirimu</span>
@@ -101,10 +102,101 @@ export default function Home() {
     );
   }
 
+  function About(){
+
+    function WhyUs(){
+
+      const reasonPoin = [
+        "Sumber lowongan pekerjaan yang komprehensif.",
+        "Tidak menggantikan peran platform rekrutmen.",
+        "Membantu memahami dan memenuhi tren pekerjaan.",
+        "Meningkatkan efisiensi pencarian dan pencocokan pelamaran kerja."
+      ]
+
+      function Point({ text }: {text: string}){
+
+        return (
+          <div className="flex w-full md:text-3xl gap-2">
+            <div className="bg-primary text-white text-center w-4 md:w-[28px] md:p-[2px] h-fit mt-1 rounded-full">
+              <PlussIcon />
+            </div>
+            <span className="font-medium">
+              { text }
+            </span>
+          </div>
+        )
+      }
+
+      return (
+        <div className="w-full h-[256px] md:h-[512px] flex justify-between">
+          <div className="flex size-[256px] md:size-[512px] relative">
+            <Image src={"/assets/img/about-1.png"} alt="why us ?" fill className="rounded-lg" />
+          </div>
+          <div className="ml-[2%] w-[56%] flex flex-col justify-between">
+            <span className="font-semibold text-3xl md:text-6xl text-primary pb-[2%]">
+              Mengapa kami ?
+            </span>
+            <div className="h-[76%] flex flex-col justify-between">
+              {reasonPoin.map((value, index) => (
+                <Point
+                  key={index}
+                  text={value}
+                />
+              ))}
+            </div>
+          </div>
+        </div>
+      );
+    }
+
+    function JoinUs(){
+      return (
+        <div className="w-full h-[256px] md:h-[512px] flex justify-between mt-12 md:mt-24">
+          <div className="ml-[2%] w-[56%] flex flex-col justify-between">
+            <span className="font-semibold text-3xl md:text-6xl text-primary rounded-md">
+              Masa depan bersama Quire
+            </span>
+            <div className="flex flex-col text-xs font-medium text-pretty md:text-2xl">
+              <span>
+                Gunakan Quire, aplikasi lowongan kerja luas, sebagai standar
+                pencarian karir Anda. Kami membantu Anda menemukan peluang kerja
+                relevan dan mengajak Anda untuk berkontribusi dalam perkembangan
+                Quire.
+              </span>
+              <span className="mt-2">
+                Kami di Quire tidak bertujuan untuk menggantikan peran platform
+                rekrutmen pekerjaan lainnya, melainkan mengajak Anda untuk
+                bermitra. Temukan pelamar kerja yang luas dan berkualitas. Mari
+                bekerja sama dengan Quire dan bersama-sama kita bangun masa
+                depan yang lebih baik.
+              </span>
+            </div>
+            <div className="text-white bg-primary rounded-sm w-fit px-2 md:px-4 py-1 md:py-2 text-xs md:text-2xl font-semibold">
+              Kontak Kami
+            </div>
+          </div>
+          <div className="flex size-[256px] md:size-[512px] relative">
+            <Image src={"/assets/img/about-2.png"} alt="why us ?" fill className="rounded-lg"/>
+          </div>
+        </div>
+      );
+    }
+
+    return(
+      <div className="block h-auto bg-altBg py-12 md:py-32 px-[5%]">
+        <WhyUs />
+        <JoinUs />
+      </div>
+    )
+  }
+
+  
+
   return (
-    <div className="w-full h-screen mb-9">
+    <div className="w-full h-screen mb-8">
       <Masthead />
       <ServiceSection />
+      <About />
     </div>
   );
 }
