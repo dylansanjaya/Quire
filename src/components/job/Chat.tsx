99706f03cf72dc7da0e38971537c180160918ec0
dylansanjaya <dylanhikmasanjaya@gmail.com>
2024-06-15 19:54:15 +0700

    Update chatbot UI

--- src/components/job/Chat.tsx
+++ src/components/job/Chat.tsx
@@ -1,10 +1,31 @@
+import Link from "next/link";
 import { Input } from "../ui/input";
+import { MdArrowOutward } from "react-icons/md";
 
 export default function ChatBot() {
   return (
-    <div>
-      <h3>Chat dengan lowongan</h3>
-      <Input placeholder="Tanyakan mengenai lowongan ini" />
+    <div className="space-y-4">
+      <h3 className="text-xl font-semibold">Chat dengan lowongan</h3>
+      <Input placeholder="Tanyakan mengenai lowongan ini" className="max-w-96"/>
+      <p className="font-semibold">Pertanyaan yang sering ditanyakan:</p>
+      <div className="space-y-2">
+        <Link href={"/jobs"} className="flex space-x-2 items-center hover:underline">
+          <p>Point point klasifikasi pekerjaan</p>
+          <MdArrowOutward />
+        </Link>
+        <Link href={"/jobs"} className="flex space-x-2 items-center hover:underline">
+          <p>Point point yang bisa di masukan ke dalam CV</p>
+          <MdArrowOutward />
+        </Link>
+        <Link href={"/jobs"} className="flex space-x-2 items-center hover:underline">
+          <p>Kesimpulan pengalaman yang dibutuhkan</p>
+          <MdArrowOutward />
+        </Link>
+        <Link href={"/jobs"} className="flex space-x-2 items-center hover:underline">
+          <p>Pengalaman yang dibutuhkan untuk pekerjaan ini</p>
+          <MdArrowOutward />
+        </Link>
+      </div>
     </div>
   );
 }
